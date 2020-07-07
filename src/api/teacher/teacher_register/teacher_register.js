e2a4db4aef8742573a96a7864a3ebfca6b7d23e9
huang <1045103201@qq.com>
2020-07-07 20:02:56 +0800

    接口数据

--- src/api/teacher/teacher_register/teacher_register.js
+++ src/api/teacher/teacher_register/teacher_register.js
@@ -10,7 +10,32 @@ export  const teacherRegister = (profile,myInstitution,teacherDetail,experiences
   return request({
     url: '/api/teacherRegister/',
     method: 'post',
-    data:{},
+    data:{
+      teachAccount:other.userName,//教师账户名
+      teachPhone:profile.phoneNumber,//教师手机号
+      teachEmail:profile.email,//教师邮箱
+      passwd:other.userPassword,//教师密码
+      teachName:profile.name,//教师姓名
+      sex:profile.sex,//教师性别
+      teachBirth:profile.birthDate,//教师出生日期
+      teachingSubject:'',//教师授课学科
+      isTeachQualifClass:other.teacherCertification,
+      certificateNum:other.certificateNumber,
+      professionalTitle:teacherDetail.title,
+      countryNature:'',
+      nationality:profile.region,
+      highestEducation:teacherDetail.qualification,
+      educationInstitution:teacherDetail.institutionWithQualification,
+      highestDegree:teacherDetail.degree,
+      degreeObtainedInstitution:teacherDetail.institutionWithDegree,
+      major:teacherDetail.major,
+      graduationDate:teacherDetail.graduatedDate,
+      workType:'',
+      idType:profile.certificateType,
+      idNum:profile.certificateNumber,
+      nativePlace:profile.nativePlace,
+      politicalStatus:profile.politicalStatus,
+    },
   })
 }
 

5d1383d7eee98f88c8efc2a22631c79407ffc6a3
huang <1045103201@qq.com>
2020-07-07 18:05:06 +0800

    教师模块接口

---
+++ src/api/teacher/teacher_register/teacher_register.js
@@ -0,0 +1,26 @@
+import request from '@/router/axios';
+
+export  const teacherRegister = (profile,myInstitution,teacherDetail,experiences,other) => {
+  console.log('////////')
+  console.log(profile)
+  console.log(myInstitution)
+  console.log(teacherDetail)
+  console.log(experiences)
+  console.log(other)
+  return request({
+    url: '/api/teacherRegister/',
+    method: 'post',
+    data:{},
+  })
+}
+
+
+export  const UploadFiles = (formData) => {
+  console.log(formData)
+  return request({
+    url: '/api/other',
+    method: 'post',
+    data: formData,
+
+  })
+}

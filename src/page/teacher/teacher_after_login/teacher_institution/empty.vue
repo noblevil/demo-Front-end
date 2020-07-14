6479afa72ab9d817cffda5b79cc51cf1406b9f17
huang <1045103201@qq.com>
2020-07-14 23:54:55 +0800

    提交本地代码

---
+++ src/page/teacher/teacher_after_login/teacher_institution/empty.vue
@@ -0,0 +1,11 @@
+<template>
+<div></div>    
+</template>
+<script>
+export default {
+    data(){
+        this.$router.push({path: "/teacher-after-login/teacher-institution"});
+        return{}
+    }
+}
+</script>
